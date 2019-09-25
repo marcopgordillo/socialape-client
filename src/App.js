f74a5d1297e5c4d76edaf3a2e2353fa675bd835d
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-09-25 10:14:47 -0400

    Add home page with scream component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 import './App.css';
+
+// Components
+import Navbar from './components/Navbar';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <div className="container">
+            <Navbar/>
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/login" component={Login}></Route>
+              <Route exact path="/signup" component={Signup}></Route>
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+function NoMatch({ location }) {
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+import Scream from '../components/Scream';
+
+class Home extends Component {
+  state = {
+    screams: null
+  }
+  
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+    ) : <p>Loading...</p>
+
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default Home
